4d0f1ff573e37719afd6dfa74c08f2a131658e6e
Sarvesh B Gugale <144622623+SarveshGugale@users.noreply.github.com>
2025-01-24 17:37:49 +0530

    Create Rds.tf
    RDS creation terraform syntax

---
+++ Rds.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
